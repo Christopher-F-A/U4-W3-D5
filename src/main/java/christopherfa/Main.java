7c1fbee487137cf04d0849d8f954dd61e2fd2f62
Christopher F A <christopher.armani095@gmail.com>
2026-03-27 15:05:13 +0100

    Add JPA entities, init persistence & README

--- src/main/java/christopherfa/Main.java
+++ src/main/java/christopherfa/Main.java
@@ -1,17 +1,17 @@
 package christopherfa;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("BibliotecaPU");
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println("Connessione riuscita e tabelle generate!");
+
+        em.close();
+        emf.close();
     }
 }
